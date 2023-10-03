e1be94a1daca8b3a5dadc13c14884a0a433dafac
bobubuntu <musgravehill@gmail.com>
2023-10-04 00:05:53 +0300

    prod sfc

--- vvd/src/App-product.vue
+++ vvd/src/App-product.vue
@@ -1,33 +1,13 @@
 <script setup>
-import { ref,onMounted } from 'vue'
-
-import HelloWorld from './components/HelloWorld.vue'
-import GridBooks from './components/Grid.vue'
-
-const searchQuery = ref('')
-const gridColumns = ['name', 'power']
-const gridData = ref([])
-
-async function fetchGridData() {
-  gridData.value = []
-  const responce = await fetch('http://vvd.ru/grid-data.json')
-  gridData.value = await responce.json()
-}
-
-onMounted(() => { fetchGridData() })
+import { ref, onMounted } from 'vue'
 
 
 </script>
 
-<template>
-  <form id="search">
-    Search <input name="query" v-model="searchQuery">
-  </form>
-  <GridBooks :data="gridData" :columns="gridColumns" :filter-key="searchQuery"></GridBooks>
-
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<template></template>
 
 <style scoped>
-
+.classLocalTplOnly {
+  color: red;
+}
 </style>
---
+++ vvd/src/components/Product.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+    id: String,
+    name: String,
+    productOptions: Array,
+})
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+.product-img{
+
+}
+</style>
