e6da953c34a4346d95958f7d8213d84baca66522
Bob-Eva-PC <musgravehill@gmail.com>
2023-10-06 20:42:52 +0300

    product.sfc

--- vvd/src/App-product.vue
+++ vvd/src/App-product.vue
@@ -1,13 +1,23 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import Product from './components/Product.vue'
 
+// TODO fetch this data 
+const productOptionColors = ref(
+  [
+    { optionId: '8c6de7c9-542d-4ada-ab10-68a4446f9548', colorName: 'red' },
+    { optionId: 'dc50f6d1-9c5a-4542-a92a-5686eb6c7a11', colorName: 'green' },
+  ]
+)
+
+const productId = ref('fe4d67b5-3ccb-4bb1-9b85-634c8badaa15')
+
+const productName = ref('Fishing reel for big game')
 
 </script>
 
-<template></template>
+<template>
+  <Product :id="productId" :name="productName" :productOptionColors="productOptionColors"></Product>
+</template>
 
-<style scoped>
-.classLocalTplOnly {
-  color: red;
-}
-</style>
+<style scoped></style>
--- vvd/src/components/Product.vue
+++ vvd/src/components/Product.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, defineProps } from 'vue'
 
 const props = defineProps({
     id: String,
     name: String,
-    productOptions: Array,
+    productOptionColors: Array,
 })
+
+const productOptionColor = ref(props.productOptionColors[0].optionId)
 
 </script>
 
 <template>
-    
+    <h1>
+        {{ name }}
+    </h1>
+
+    {{ productOptionColor }}
+
+    <span v-for="({ optionId, colorName }, idx) in productOptionColors" :key="optionId">
+        <input type="radio" :id="optionId" :value="optionId" v-model="productOptionColor" />
+        <label :for="optionId">{{ colorName }}</label>
+    </span>
+
+    <!-- 
+    v-for="({ optionId, colorName }, idx) in productOptionColors"
+    :checked="idx === 0 ? true : false"
+    -->
 </template>
 
 <style scoped>
-.product-img{
-
-}
+.product-img {}
 </style>
