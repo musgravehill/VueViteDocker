95c7c56ec29d150857a3b290c166784d16e8ed4b
Bob-Eva-PC <musgravehill@gmail.com>
2023-10-06 21:32:43 +0300

    Product.sfc imgs and radio options

--- vvd/src/App-product.vue
+++ vvd/src/App-product.vue
@@ -3,21 +3,18 @@ import { ref, onMounted } from 'vue'
 import Product from './components/Product.vue'
 
 // TODO fetch this data 
-const productOptionColors = ref(
+const productId = ref('fe4d67b5-3ccb-4bb1-9b85-634c8badaa15')
+const productName = ref('Fishing reel for big game')
+const productOptions = ref(
   [
-    { optionId: '8c6de7c9-542d-4ada-ab10-68a4446f9548', colorName: 'red' },
-    { optionId: 'dc50f6d1-9c5a-4542-a92a-5686eb6c7a11', colorName: 'green' },
+    { optionId: '8c6de7c9-542d-4ada-ab10-68a4446f9548', colorName: 'red', imgUrl: '/product/red.png' },
+    { optionId: 'dc50f6d1-9c5a-4542-a92a-5686eb6c7a11', colorName: 'green', imgUrl: '/product/green.png' },
   ]
 )
-
-const productId = ref('fe4d67b5-3ccb-4bb1-9b85-634c8badaa15')
-
-const productName = ref('Fishing reel for big game')
-
 </script>
 
 <template>
-  <Product :id="productId" :name="productName" :productOptionColors="productOptionColors"></Product>
+  <Product :id="productId" :name="productName" :productOptions="productOptions"></Product>
 </template>
 
 <style scoped></style>
--- vvd/src/components/Product.vue
+++ vvd/src/components/Product.vue
@@ -4,10 +4,13 @@ import { ref, computed, defineProps } from 'vue'
 const props = defineProps({
     id: String,
     name: String,
-    productOptionColors: Array,
+    productOptions: Array,
 })
 
-const productOptionColor = ref(props.productOptionColors[0].optionId)
+const modelProductOption = ref(props.productOptions[0].optionId)
+const imgUrl = computed(() =>
+    props.productOptions.find((option) => option.optionId == modelProductOption.value).imgUrl
+)
 
 </script>
 
@@ -16,12 +19,15 @@ const productOptionColor = ref(props.productOptionColors[0].optionId)
         {{ name }}
     </h1>
 
-    {{ productOptionColor }}
+    <img :src="imgUrl" :alt="name" class="product-img">
 
-    <span v-for="({ optionId, colorName }, idx) in productOptionColors" :key="optionId">
-        <input type="radio" :id="optionId" :value="optionId" v-model="productOptionColor" />
-        <label :for="optionId">{{ colorName }}</label>
+    <span v-for="productOption in productOptions" :key="productOption.optionId" class="product-radio">
+        <input type="radio" :id="productOption.optionId" :value="productOption.optionId" v-model="modelProductOption" />
+        <label :for="productOption.optionId">{{ productOption.colorName }}</label>
     </span>
+
+    <br><br><br>
+    Selected optionId: {{ modelProductOption }}
 
     <!-- 
     v-for="({ optionId, colorName }, idx) in productOptionColors"
@@ -30,5 +36,12 @@ const productOptionColor = ref(props.productOptionColors[0].optionId)
 </template>
 
 <style scoped>
-.product-img {}
+.product-img {
+    max-height: 256px;
+    max-width: 256px;
+}
+
+.product-radio {
+    margin-right: 20px;
+}
 </style>
