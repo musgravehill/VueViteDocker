61db7a509d49088fb1119b6dc91d76f431158fe2
Bob-Eva-PC <musgravehill@gmail.com>
2023-10-07 20:50:57 +0300

    simple cart

--- vvd/src/App-product.vue
+++ vvd/src/App-product.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useFetch } from './components/fetch.js'
 import Product from './components/Product.vue'
 
@@ -9,14 +9,38 @@ import Product from './components/Product.vue'
 //const { data, error } = useFetch(() => `/posts/${someId}`)
 const { data: products, error } = useFetch('/products.json')
 
+
+
 function onEventAddToCart(payload) {
-  console.log(payload)
+  addToCart(payload.id, payload.optionId)
+}
+
+const cartProducts = ref([])
+
+const cartTotalQty = computed(() => {
+  return cartProducts.value.reduce((totalQty, item) => (totalQty + item.quantity), 0)
+})
+
+function addToCart(id, optionId) {
+  let productInCart = cartProducts.value.find((item) => {
+    return (item.id === id && item.optionId === optionId)
+  });
+  if (productInCart) {
+    productInCart.quantity += 1;
+    return;
+  }
+  cartProducts.value.push({ id: id, optionId: optionId, quantity: 1 });
 }
 
 </script>
 
 <template>
-  12345
+  <div class="cart-total-qty">
+    Cart qty: {{ cartTotalQty }}
+  </div>
+  <div class="cart-data">
+    {{ cartProducts }}
+  </div>
   <div class="products">
     <div v-for="product in products">
       <Product @event-add-to-cart="onEventAddToCart" :id="product.productId" :name="product.productName" :options="product.options"></Product>
@@ -25,6 +49,16 @@ function onEventAddToCart(payload) {
 </template>
 
 <style scoped>
+.cart-total-qty {
+  font-size: 22px;
+  color: blueviolet;
+  font-weight: bold;
+}
+
+.cart-data {
+  font-size: 10px;
+}
+
 .products {
   display: flex;
 }
